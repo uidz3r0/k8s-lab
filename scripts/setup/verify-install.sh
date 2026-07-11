1e2d05b164684d5aa3896dbef0ca4aafd80b6fe4
AJ <allan@aj.lan>
2026-07-11 12:08:36 +1000

    Added files

--- scripts/setup/verify-install.sh
+++ scripts/setup/verify-install.sh
@@ -1,1 +1,56 @@
-/home/allan/k8s/week7-cluster-operations/W7D1/scripts/common/verify-install.sh
+#!/bin/bash
+
+echo "===== containerd ====="
+containerd --version
+systemctl is-active containerd
+
+echo
+echo "===== kubelet ====="
+kubelet --version
+systemctl is-enabled kubelet
+
+echo
+echo "===== CRI ====="
+crictl info >/dev/null && crictl version && echo "CRI OK" || echo "crictl not installed"
+
+echo
+echo "===== OCI Runtime ====="
+if command -v runc >/dev/null 2>&1; then
+    runc --version | head -1
+else
+    echo "runc not installed"
+fi
+
+echo
+echo "===== cgroup ====="
+grep SystemdCgroup /etc/containerd/config.toml
+
+echo
+echo "===== swap ====="
+swapon --show
+
+echo
+echo "===== kernel modules ====="
+lsmod | grep overlay
+lsmod | grep br_netfilter
+
+echo
+echo "===== sysctl ====="
+sysctl net.bridge.bridge-nf-call-iptables
+sysctl net.ipv4.ip_forward
+
+echo
+echo "===== Versions ====="
+kubeadm version
+kubectl version --client
+
+echo
+echo "===== Firewall ====="
+
+if command -v firewall-cmd >/dev/null 2>&1; then
+    systemctl is-active firewalld
+elif command -v ufw >/dev/null 2>&1; then
+    systemctl is-active ufw
+else
+    echo "No supported firewall service detected."
+fi
