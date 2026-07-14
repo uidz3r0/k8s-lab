feda2470bcf80f50eedda068ce7cb4d91a91bc20
AJ <allan@aj.lan>
2026-07-14 16:25:40 +1000

    w7d5

--- scripts/cluster/check-etcd.sh
+++ scripts/cluster/check-etcd.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-export ETCDCTL_API=3
+kubectl exec -n kube-system etcd-luke -- \
+  etcdctl \
+  --endpoints=https://127.0.0.1:2379 \
+  --cacert=/etc/kubernetes/pki/etcd/ca.crt \
+  --cert=/etc/kubernetes/pki/etcd/healthcheck-client.crt \
+  --key=/etc/kubernetes/pki/etcd/healthcheck-client.key \
+  member list
 
-sudo etcdctl \
---cacert=/etc/kubernetes/pki/etcd/ca.crt \
---cert=/etc/kubernetes/pki/etcd/server.crt \
---key=/etc/kubernetes/pki/etcd/server.key \
-member list
+kubectl exec -n kube-system etcd-han -- \
+  etcdctl \
+  --endpoints=https://127.0.0.1:2379 \
+  --cacert=/etc/kubernetes/pki/etcd/ca.crt \
+  --cert=/etc/kubernetes/pki/etcd/healthcheck-client.crt \
+  --key=/etc/kubernetes/pki/etcd/healthcheck-client.key \
+  member list  
